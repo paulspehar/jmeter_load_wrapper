5ee9ed319c2f46f976ab354e7f166d40daff5271
paulspehar <pspehar@gmail.com>
2021-08-18 16:31:48 -0700

    initial commit

---
+++ jmeter_load.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+# jmeter_load.py - starts jmeter with paramaters from config file. Uploads report.
+
+import logging
+import time
+
+logging.debug('Starting program...')
+
+
+# parse paramater file
+
+# mark script start time
+
+# set jmeter java options
+
+# launch jmeter with options
+
+# mark script end time
+
+# send report and logs to report server
+
+# clean up and compress results
+
